b8acb61ef381ad857b9a5d40c501400ae9ac4d9c
samithspalan <samithpoojary12345@gmail.com>
2025-01-14 17:01:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let hour_hand=document.querySelector('.hour-hand')
+let min_hand=document.querySelector('.min-hand')
+let sec_hand=document.querySelector('.sec-hand')
+
+setInterval(()=>{
+    let time=new Date();
+    let h=time.getHours();
+    let m=time.getMinutes();
+    let s=time.getSeconds();
+    let hrotation=30*h+h/2;
+    let mrotation=6*m;
+    let srotation=6*s;
+    hour_hand.style.transform = `rotate(${hrotation}deg)`;
+    min_hand.style.transform = `rotate(${mrotation}deg)`;
+    sec_hand.style.transform = `rotate(${srotation}deg)`;
+
+
+
+
+})
